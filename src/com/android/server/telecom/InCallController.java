1f9734dfda242616da070a1c704c614dbedef99e
jhonboy121 <alfredmathew05@gmail.com>
2022-03-16 22:04:48 +0530

    InCallController: use click & double click effect for connect and disconnect
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- src/com/android/server/telecom/InCallController.java
+++ src/com/android/server/telecom/InCallController.java
@@ -49,6 +49,7 @@ import android.os.RemoteException;
 import android.os.Trace;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.provider.Settings;
 import android.telecom.CallAudioState;
@@ -97,11 +98,10 @@ public class InCallController extends CallsManagerListenerBase implements
     public static final int IN_CALL_SERVICE_NOTIFICATION_ID = 3;
     private AnomalyReporterAdapter mAnomalyReporter = new AnomalyReporterAdapterImpl();
 
-    private static final long[] INCALL_VIBRATION_PATTERN = {
-            100,
-            200,
-            0,
-    };
+    private static final VibrationEffect CALL_CONNECT_EFFECT =
+            VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK);
+    private static final VibrationEffect CALL_DISCONNECT_EFFECT =
+            VibrationEffect.createPredefined(VibrationEffect.EFFECT_DOUBLE_CLICK);
 
     /**
      * Anomaly Report UUIDs and corresponding error descriptions specific to InCallController.
@@ -1250,6 +1250,8 @@ public class InCallController extends CallsManagerListenerBase implements
     private ArraySet<String> mActiveCarrierPrivilegedApps = new ArraySet<>();
     private FeatureFlags mFeatureFlags;
 
+    private final Vibrator mVibrator;
+
     public InCallController(Context context, TelecomSystem.SyncRoot lock, CallsManager callsManager,
             SystemStateHelper systemStateHelper, DefaultDialerCache defaultDialerCache,
             Timeouts.Adapter timeoutsAdapter, EmergencyCallHelper emergencyCallHelper,
@@ -1257,6 +1259,7 @@ public class InCallController extends CallsManagerListenerBase implements
         mContext = context;
         mAppOpsManager = context.getSystemService(AppOpsManager.class);
         mSensorPrivacyManager = context.getSystemService(SensorPrivacyManager.class);
+        mVibrator = context.getSystemService(Vibrator.class);
         mLock = lock;
         mCallsManager = callsManager;
         mSystemStateHelper = systemStateHelper;
@@ -1549,12 +1552,18 @@ public class InCallController extends CallsManagerListenerBase implements
             Settings.System.VIBRATE_ON_DISCONNECT, 0, UserHandle.USER_CURRENT) >= 1;
 
         if (oldState == CallState.DIALING && newState == CallState.ACTIVE && vibrateOnConnect) {
-            vibrate(INCALL_VIBRATION_PATTERN);
+            vibrate(CALL_CONNECT_EFFECT);
         } else if (oldState == CallState.ACTIVE && newState == CallState.DISCONNECTED
                 && vibrateOnDisconnect) {
-            vibrate(INCALL_VIBRATION_PATTERN);
+            vibrate(CALL_DISCONNECT_EFFECT);
         }
         updateCall(call);
+    }
+
+    public void vibrate(VibrationEffect effect) {
+        if (mVibrator.hasVibrator()) {
+            mVibrator.vibrate(effect);
+        }
     }
 
     @Override
@@ -2977,9 +2986,5 @@ public class InCallController extends CallsManagerListenerBase implements
             }
         }
         return false;
-    }
-
-    public void vibrate(long[] pattern) {
-        ((Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE)).vibrate(pattern, -1);
     }
 }
