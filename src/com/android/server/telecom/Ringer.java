f057c3a9c4108b9e2e4abd7d6e3855d3d447f37d
NurKeinNeid <mralexman3000@gmail.com>
2023-12-09 14:29:17 +0100

    Merge tag 'android-14.0.0_r17' of https://android.googlesource.com/platform/packages/services/Telecomm into HEAD
    Android 14.0.0 Release 17 (UQ1A.231205.015)

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -488,7 +488,7 @@ public class Ringer {
             };
             deferBlockOnRingingFuture = true;  // Run in vibrationLogic.
             if (ringtoneSupplier != null) {
-                mRingtonePlayer.play(ringtoneSupplier, afterRingtoneLogic);
+                mRingtonePlayer.play(ringtoneSupplier, afterRingtoneLogic, isHfpDeviceAttached);
             } else {
                 afterRingtoneLogic.accept(/* ringtone= */ null, /* stopped= */ false);
             }
